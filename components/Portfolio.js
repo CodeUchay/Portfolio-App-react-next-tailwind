72b8a2a03f89b6be6dff2a97c1e186e6d54a2d6c
Uche Odinamba <uchay2x@icloud.com>
2023-07-18 15:47:39 +0200

    Projects Update

--- components/Portfolio.js
+++ components/Portfolio.js
@@ -7,6 +7,9 @@ import web4 from "../images/app4.png";
 import web5 from "../images/app5.png";
 import web6 from "../images/app6.png";
 import web7 from "../images/app00.png";
+import web8 from "../images/app7.png";
+import web9 from "../images/app8.png";
+import web10 from "../images/app9.png";
 
 export default function Portfolio() {
   return (
@@ -17,20 +20,24 @@ export default function Portfolio() {
           Portofolio
         </h3>
         <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-200">
-          These are a list of my personal project as a Software Engineer. You can
-          find projects on other Software Applications on my
+          These are a list of my personal project as a Software Engineer. You
+          can find projects on other Software Applications on my
           <span className="text-teal-500"> Github </span>
           for example{" "}
           <span className="text-teal-500">
             {" "}
-            Mobile Applications, Desktop Applications, Machine Learning Projects etc{" "}
+            Mobile Applications, Web Applications, Machine Learning Projects etc{" "}
           </span>
           I work daily on new projects so feel free to contact me if you want a
           collaboration.
         </p>
       </div>
       <div className="flex flex-col gap-10 py-10 lg:flex-row lg:flex-wrap">
-        <div className="basis-1/3 flex-1 text-white hover:bg-gray-800 hover:shadow-lg hover:shadow-teal-600 transition duration-500 ease-in-out hover:scale-105">
+        {/** Portfolio App */}
+        <a
+          href="https://react-portofolio-tailwind-beta.vercel.app"
+          className="basis-1/3 flex-1 text-white hover:bg-gray-800 hover:shadow-lg hover:shadow-teal-600 transition duration-500 ease-in-out hover:scale-105"
+        >
           <Image
             className="rounded-lg object-cover"
             width={"100%"}
@@ -39,11 +46,16 @@ export default function Portfolio() {
             src={web5}
             alt="Portfolio Image"
           />
-          <p className="bottom-0 left-0 right-0 px-4 py-2 font-bold text-center">
-          My Portfolio App
-          </p>
-        </div>
-        <div className="basis-1/3 flex-1 text-white hover:bg-gray-800 hover:shadow-lg hover:shadow-teal-600 transition duration-500 ease-in-out hover:scale-105 hover:text-white">
+          <div className="bottom-0 left-0 right-0 px-4 py-2 text-center">
+            <p className="font-bold"> My Portfolio App</p>
+            <span>React.js, Next.js, Tailwind CSS</span>
+          </div>
+        </a>
+        {/** Uche's Diary */}
+        <a
+          href="https://blog-app-react-firebase.vercel.app"
+          className="basis-1/3 flex-1 text-white hover:bg-gray-800 hover:shadow-lg hover:shadow-teal-600 transition duration-500 ease-in-out hover:scale-105 hover:text-white"
+        >
           <Image
             className="rounded-lg object-cover"
             width={"100%"}
@@ -52,26 +64,34 @@ export default function Portfolio() {
             alt="Portfolio Image"
             src={web6}
           />
-          <p className="bottom-0 left-0 right-0 px-4 py-2 font-bold text-center">
-            
-            Uche's Diary (Blog App)
-          </p>
-        </div>
-
-        <div className="basis-1/3 flex-1 text-white hover:bg-gray-800 hover:shadow-lg hover:shadow-teal-600 transition duration-500 ease-in-out hover:scale-105">
+          <div className="bottom-0 left-0 right-0 px-4 py-2 text-center">
+            <p className="font-bold"> Uche's Diary (Blog App) </p>
+            <span>React.js, Firebase, Mongo DB, REST API</span>
+          </div>
+        </a>
+        {/** Infinity */}
+        <a
+          href="https://infinity-five-snowy.vercel.app"
+          className="basis-1/3 flex-1 text-white hover:bg-gray-800 hover:shadow-lg hover:shadow-teal-600 transition duration-500 ease-in-out hover:scale-105"
+        >
           <Image
             className="rounded-lg object-cover"
             width={"100%"}
             height={"60%"}
             layout="responsive"
             alt="Portfolio Image"
-            src={web7}
+            src={web8}
           />
-          <p className="bottom-0 left-0 right-0 px-4 py-2 font-bold text-center">
-            Ecommerce Dashboard
-          </p>
-        </div>
-        <div className="basis-1/3 flex-1  text-white hover:bg-gray-800 hover:shadow-lg hover:shadow-teal-600 transition duration-500 ease-in-out hover:scale-105">
+          <div className="bottom-0 left-0 right-0 px-4 py-2 text-center">
+            <p className="font-bold"> Infinity Tech (Product Landing Page) </p>
+            <span>React.js, Tailwind CSS</span>
+          </div>
+        </a>
+        {/** Trivia */}
+        <a
+          href="https://trivia-app-cyan.vercel.app"
+          className="basis-1/3 flex-1  text-white hover:bg-gray-800 hover:shadow-lg hover:shadow-teal-600 transition duration-500 ease-in-out hover:scale-105"
+        >
           <Image
             className="rounded-lg object-cover"
             width={"100%"}
@@ -80,11 +100,16 @@ export default function Portfolio() {
             alt="Portfolio Image"
             src={web2}
           />
-          <p className="bottom-0 left-0 right-0 px-4 py-2 font-bold text-center">
-            Trivia Application
-          </p>
-        </div>
-        <div className="basis-1/3 flex-1 text-white hover:bg-gray-800 hover:shadow-lg hover:shadow-teal-600 transition duration-500 ease-in-out hover:scale-105">
+          <div className="bottom-0 left-0 right-0 px-4 py-2 text-center">
+            <p className="font-bold"> Trivia App </p>
+            <span>HTML, CSS, JavaScript</span>
+          </div>
+        </a>
+        {/** To-do List App */}
+        <a
+          href="https://todo-list-app-uche.vercel.app"
+          className="basis-1/3 flex-1 text-white hover:bg-gray-800 hover:shadow-lg hover:shadow-teal-600 transition duration-500 ease-in-out hover:scale-105"
+        >
           <Image
             className="rounded-lg object-cover"
             width={"100%"}
@@ -93,11 +118,34 @@ export default function Portfolio() {
             alt="Portfolio Image"
             src={web1}
           />
-          <p className="bottom-0 left-0 right-0 px-4 py-2 font-bold text-center">
-            To-do List App
-          </p>
-        </div>
-        <div className="basis-1/3 flex-1 text-white hover:bg-gray-800 hover:shadow-lg hover:shadow-teal-600 transition duration-500 ease-in-out hover:scale-105">
+          <div className="bottom-0 left-0 right-0 px-4 py-2 text-center">
+            <p className="font-bold"> To-do List App </p>
+            <span>HTML, CSS, JavaScript</span>
+          </div>
+        </a>
+        {/** To-do List App */}
+        <a
+          href="https://to-do-list-mauve-ten.vercel.app"
+          className="basis-1/3 flex-1 text-white hover:bg-gray-800 hover:shadow-lg hover:shadow-teal-600 transition duration-500 ease-in-out hover:scale-105"
+        >
+          <Image
+            className="rounded-lg object-cover"
+            width={"100%"}
+            height={"60%"}
+            layout="responsive"
+            alt="Portfolio Image"
+            src={web9}
+          />
+          <div className="bottom-0 left-0 right-0 px-4 py-2 text-center">
+            <p className="font-bold"> Ol'Task (To-do List App) </p>
+            <span> Typescript.js, Tailwind CSS </span>
+          </div>
+        </a>
+        {/** Event Page */}
+        <a
+          href="https://event-project-esig.vercel.app"
+          className="basis-1/3 flex-1 text-white hover:bg-gray-800 hover:shadow-lg hover:shadow-teal-600 transition duration-500 ease-in-out hover:scale-105"
+        >
           <Image
             className="rounded-lg object-cover"
             width={"100%"}
@@ -106,10 +154,65 @@ export default function Portfolio() {
             alt="Portfolio Image"
             src={web3}
           />
-          <p className="bottom-0 left-0 right-0 px-4 py-2 font-bold text-center">
-            Event Page
-          </p>
-        </div>
+          <div className="bottom-0 left-0 right-0 px-4 py-2 text-center">
+            <p className="font-bold"> Event Page </p>
+            <span> HTML, CSS, JavaScript, PHP, MYSQL </span>
+          </div>
+        </a>
+        {/** Ecommerce Dashboard */}
+        <a
+          href="https://chefkim-admin.vercel.app"
+          className="basis-1/3 flex-1 text-white hover:bg-gray-800 hover:shadow-lg hover:shadow-teal-600 transition duration-500 ease-in-out hover:scale-105"
+        >
+          <Image
+            className="rounded-lg object-cover"
+            width={"100%"}
+            height={"60%"}
+            layout="responsive"
+            alt="Portfolio Image"
+            src={web7}
+          />
+          <div className="bottom-0 left-0 right-0 px-4 py-2 text-center">
+            <p className="font-bold"> Ecommerce Admin Page </p>
+            <span> React.js, Firebase, Axios, Express, REST </span>
+          </div>
+        </a>
+        {/** Chef Kim Restaurant */}
+        <a
+          href="https://chefkimkitchen.vercel.app"
+          className="basis-1/3 flex-1 text-white hover:bg-gray-800 hover:shadow-lg hover:shadow-teal-600 transition duration-500 ease-in-out hover:scale-105"
+        >
+          <Image
+            className="rounded-lg object-cover"
+            width={"100%"}
+            height={"60%"}
+            layout="responsive"
+            alt="Portfolio Image"
+            src={web10}
+          />
+          <div className="bottom-0 left-0 right-0 px-4 py-2 text-center">
+            <p className="font-bold"> Chef Kim Restaurant </p>
+            <span> React.js, Tailwind CSS </span>
+          </div>
+        </a>
+        {/** Calculator */}
+        <a
+          href="https://chefkim-admin.vercel.app"
+          className="basis-1/3 flex-1 text-white hover:bg-gray-800 hover:shadow-lg hover:shadow-teal-600 transition duration-500 ease-in-out hover:scale-105"
+        >
+          <Image
+            className="rounded-lg object-cover"
+            width={"100%"}
+            height={"60%"}
+            layout="responsive"
+            alt="Portfolio Image"
+            src={web4}
+          />
+          <div className="bottom-0 left-0 right-0 px-4 py-2 text-center">
+            <p className="font-bold"> Calculator </p>
+            <span> HTML, CSS, JavaScript </span>
+          </div>
+        </a>
       </div>
     </section>
   );
--- components/Services.js
+++ components/Services.js
@@ -14,11 +14,11 @@ export default function Services() {
         </h3>
 
         <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-200">
-          As a self-taught programmer my dream is to keep creating projects as a full stack developer and
-          software engineer, I've worked with an IT service
+          I create projects  as a 
+          software engineer and full stack developer , I've worked with an IT service
           <span className="text-teal-500"> company </span>
-          and also handled websites for some <span className="text-teal-500">firms </span>
-          and i am currently on track to become a software engineer to grow further.
+          and also handled websites for some <br/> <span className="text-teal-500">firms </span>
+          and i am currently on track to become a machine learning engineer to grow further.
         </p>
         <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-200">
           Asides this, i am a business owner, fashion designer and a model
@@ -27,7 +27,7 @@ export default function Services() {
       <div className="lg:flex gap-10">
         <div className="text-center shadow-lg p-10 rounded-xl my-10 flex-1  dark:text-gray-200 items-center text-lg  hover:shadow-lg hover:shadow-teal-600 transition duration-500 ease-in-out hover:scale-105 dark:bg-teal-600">
           <Image src={design} alt="Designer" width={100} height={100} />
-          <h3 className="text-lg font-medium pt-8 pb-2  ">Build your design</h3>
+          <h3 className="text-lg font-medium pt-8 pb-2">Build your design</h3>
           <p className="py-2">
             Creating elegant designs suited for your needs following core design
             theory.
